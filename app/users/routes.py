b9f1b7cddc23eb4734c92bb596d22eeef68812e1
jerichpi <64188069+jerichpi@users.noreply.github.com>
2020-05-20 20:37:23 +0800

    ECQ Update

--- app/users/routes.py
+++ app/users/routes.py
@@ -3,21 +3,39 @@ from forms import LoginForm, PasswordForm, ProfileForm
 
 bp = Blueprint('user', __name__, template_folder='templates')
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "ticket@sample.com" and form.password.data == "user@123":
+            flash(f'Welcome to HPI Ticketing System!', 'success')
+            return redirect(url_for('user.home'))
+        else:
+            flash(f'Error login. Please check email and password.', 'danger')
+
     return render_template('user-login.html', form=form)
 
+@bp.route('/profile', methods=['GET', 'POST'])
+def profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        flash(f'Hi {form.firstname.data}, your information was updated successfully.', 'success')
+
+    return render_template('user-profile.html', form=form)
+
+@bp.route('/change-password', methods=['GET', 'POST'])
+def change_pass():
+    form = PasswordForm()
+    if form.validate_on_submit():
+        flash(f'Your password was updated successfully.', 'success')
+        return redirect(url_for('user.home'))
+    
+    return render_template('user-change-pass.html', form=form)
+
 @bp.route('/forgot-password')
-def forgot_pass():
+def forgot_pass():  
     return render_template('user-forgot-password.html')
 
 @bp.route('/home', methods=['GET', 'POST'])
 def home():
-    passform = PasswordForm()
-    proform = ProfileForm()
-    if proform.validate_on_submit():
-        flash(f'Hi {proform.firstname.data}, your info was successfully updated.', 'success')
-        return redirect(url_for('user.home'))
-        
-    return render_template('user-home.html', passform=passform, proform=proform)
+    return render_template('user-home.html')
